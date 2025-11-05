86607ac536d755068cdc84448a8a1eb0e6d087b1
Waseda_Maruyama <marukei111@suou.waseda.jp>
2025-11-05 13:50:24 +0900

    complete_sisaku1

--- sisaku/main.py
+++ sisaku/main.py
@@ -2,8 +2,7 @@ from datetime import datetime
 from dateutil import tz
 import jquantsapi
 
-my_mail_address:str = "*****"
-my_password: str = "*****"
+
 cli = jquantsapi.Client(mail_address=my_mail_address, password=my_password)
 df = cli.get_price_range(
     start_dt=datetime(2022, 7, 25, tzinfo=tz.gettz("Asia/Tokyo")),
